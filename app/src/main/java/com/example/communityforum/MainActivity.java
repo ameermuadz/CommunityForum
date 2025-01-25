00670db6584b225ec432e53e8a1e9f912f66a4cf
ameermuadz <ameer.muadz2004@gmail.com>
2025-01-25 13:38:40 +0800

    change bookmarks to bookmarks2

--- app/src/main/java/com/example/communityforum/MainActivity.java
+++ app/src/main/java/com/example/communityforum/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements PostDetailsActivi
         });
 
         btnBookmarks.setOnClickListener(v -> {
-            currentPage = "bookmarks";
+            currentPage = "bookmarks2";
             updateButtonColors();
             listenForBookmarkedPosts();
         });
